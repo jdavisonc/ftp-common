a7697e3683a62f4b5346078e76a4b2d9a48b7bb3
Jorge Davison <jdavisonc@gmail.com>
2012-08-11 16:52:23 -0300

    Add license headers

--- src/main/java/com/superdownloader/common/ftp/filter/DirectoryFileFilter.java
+++ src/main/java/com/superdownloader/common/ftp/filter/DirectoryFileFilter.java
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ * DirectoryFileFilter.java
+ * 
+ * Copyright (c) 2012 Team SeedBoxer.
+ * 
+ * This file is part of SeedBoxer FTPCommon.
+ * 
+ * SeedBoxer FTPCommon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SeedBoxer FTPCommon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SeedBoxer FTPCommon.  If not, see <http ://www.gnu.org/licenses/>.
+ ******************************************************************************/
 /**
  * 
  */
